24aa61d718b8af1e3ab1f285f6d2f3bdc6b0ce53
Ricardo Guerra <RikkuG491@gmail.com>
2024-04-24 22:38:39 -0400

    Added solution code for Deliverables

---
+++ src/index.js
@@ -0,0 +1,44 @@
+// Solution code for Deliverables
+
+// Deliverable # 1
+const greeting = "Welcome to Flatburger"
+
+// Deliverable # 2
+const num1 = 7
+
+// Deliverable # 3
+let num2 = "14"
+
+// Deliverable # 4
+console.log(typeof num2)
+
+// Deliverable # 5
+num2 = Number(num2)
+
+// Deliverable # 6
+console.log(typeof num2)
+
+// Deliverable # 7
+const sum = num1 + num2
+
+// Deliverable # 8
+const sumString = `${num1} + ${num2} = ${sum}`
+
+// Deliverable # 9
+if(num1 === 7 || num1 === 49){
+    console.log("That's a lucky number!")
+}
+
+// Deliverable # 10
+else if(num1 > 7 && num1 < 49){
+    console.log("That might be a lucky number!")
+}
+
+// Deliverable # 11
+else{
+    console.log("That's not a lucky number.")
+}
+
+// Deliverable # 12
+const luckyPhrase = num1 === 7 && num2 === 7 ? "Lucky Sevens!" : "Better luck next time"
+console.log(luckyPhrase)
